b9b0540c232fe0099b884d86aec39dabbcddbbeb
R0rt1z2 <rogerortizleal@gmail.com>
2021-08-30 14:13:33 +0200

    s5030: shim more GraphicBuffer symbols for libmmsdkservice.feature.so
    Change-Id: I12838a97fde08c3ff98113db794a120a8a8ddc0c

--- libshims/ui_shim.cpp
+++ libshims/ui_shim.cpp
@@ -1,5 +1,12 @@
+#include <gui/BufferItem.h>
+#include <gui/IConsumerListener.h>
+#include <gui/IGraphicBufferConsumer.h>
 #include <ui/GraphicBufferMapper.h>
+#include <gui/BufferItemConsumer.h>
 #include <ui/Rect.h>
+
+#include <log/log.h>
+#define LOG_TAG "BufferQueue_shim"
 
 extern "C" {
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t, uint32_t, const android::Rect&, void**);
@@ -18,3 +25,43 @@ extern "C" {
     }
 }
 
+extern "C" void _ZN7android22IGraphicBufferConsumerC2Ev();
+
+// The constructor was replaced in Android 6.0
+extern "C" void _ZN7android22IGraphicBufferConsumer10BufferItemC1Ev() {
+    return _ZN7android22IGraphicBufferConsumerC2Ev();
+}
+
+namespace android {
+
+// The function was removed in Android 6.0
+extern "C" status_t _ZN7android18BufferItemConsumer20setDefaultBufferSizeEjj(uint32_t w, uint32_t h) {
+    ALOGD("%s: deprecated call. w -> %lu, h -> %lu", __func__, w, h);
+    return 0;
+}
+
+// The function was removed in Android 6.0
+extern "C" status_t _ZN7android18BufferItemConsumer22setDefaultBufferFormatEj(uint32_t defaultformat) {
+    ALOGD("%s: deprecated call. defaultformat -> %lu", __func__, defaultformat);
+    return 0;
+}
+
+extern "C" status_t _ZN7android18BufferItemConsumer13acquireBufferEPNS_10BufferItemExb(BufferItem *item,
+        nsecs_t presentWhen, bool waitForFence);
+
+// The function was renamed in Android 6.0
+extern "C" status_t _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb(BufferItem *item,
+    nsecs_t presentWhen, bool waitForFence) {
+        return _ZN7android18BufferItemConsumer13acquireBufferEPNS_10BufferItemExb(item, presentWhen, waitForFence);
+}
+
+extern "C" status_t _ZN7android18BufferItemConsumer13releaseBufferERKNS_10BufferItemERKNS_2spINS_5FenceEEE(const BufferItem &item,
+    const sp<Fence>& releaseFence);
+
+// The function was renamed in Android 6.0
+extern "C" status_t _ZN7android18BufferItemConsumer13releaseBufferERKNS_22IGraphicBufferConsumer10BufferItemERKNS_2spINS_5FenceEEE(const BufferItem &item,
+    const sp<Fence>& releaseFence) {
+        return _ZN7android18BufferItemConsumer13releaseBufferERKNS_10BufferItemERKNS_2spINS_5FenceEEE(item, releaseFence);
+}
+
+} // namespace android
